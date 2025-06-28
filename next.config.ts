6f0f8c0f14fd3c4c4ec01fc19b487902e60cdbcd
D/One Repo <d-one.repo@daedalos.io>
2025-06-28 23:58:28 +0200

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from 'next'
+const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+}
+export default nextConfig
